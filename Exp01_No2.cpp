c0f43a53051812635246a7a0109066de2891a333
LionelZQY <112601009+LionelZQY@users.noreply.github.com>
2022-09-04 16:49:56 +0800

    Add files via upload

---
+++ Exp01_No2.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int a, b, sum;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("%d", sum);
+	return 0;
+}
---
+++ 实验一/Exp01_No3.cpp
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int num1, num2;
+//	printf("请输入比较的两个数");
+//	scanf("%d %d", &num1, &num2);
+//	if (num1 > num2) {
+//		printf("较大的数为%d", num1);
+//	}
+//	else {
+//		printf("较大的数为%d", num2);
+//	}
+//	return 0;
+//}
